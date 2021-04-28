7d7d2d83fa902cd497092f58c91a2674dc181379
haranba <barak.haran@gmail.com>
2021-04-28 15:54:46 +0300

    6

---
+++ CV/js1.js
@@ -0,0 +1,38 @@
+// button with function that change attribute.
+function click1() {
+    document.body.innerHTML = document.body.innerHTML.replace("big dream", "Make the right thing to do the easy thing to do.");
+}
+
+function click2() {
+    document.body.innerHTML = document.body.innerHTML.replace("Make the right thing to do the easy thing to do.", "big dream");
+}
+
+var ck_name = /^[A-Za-z0-9 ]{3,20}$/;
+var ck_email = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+
+function ValidateName(form) {
+    var name = form.name.value;
+    var errors = [];
+
+    if (!ck_name.test(name)) {
+        errors[errors.length] = "You valid Name .";
+    }
+    if (!ck_email.test(email)) {
+        errors[errors.length] = "You must enter a valid email address.";
+    }
+    if (errors.length > 0) {
+
+        reportErrors(errors);
+        return false;
+    }
+    return true;
+}
+
+function reportErrors(errors) {
+    var msg = "Please Enter Valide Data...\n";
+    for (var i = 0; i < errors.length; i++) {
+        var numError = i + 1;
+        msg += "\n" + numError + ". " + errors[i];
+    }
+    alert(msg);
+}
